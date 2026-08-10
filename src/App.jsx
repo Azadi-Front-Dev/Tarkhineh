eb1765a7b68dc9f0bb77397bc6e0914224bd870b
Ali <ali.azadi.dev@gmail.com>
2026-08-10 13:07:10 +0330

    feat : install and initialize react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import { useRoutes } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 
+const routes = [
+  { path: "/", element: <h1 className="text-center font-bold">home</h1> },
+  { path: "/", element: <></> },
+  { path: "/", element: <></> },
+];
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className=""></div>;
+  const router = useRoutes(routes);
+  return <div className="">{router}</div>;
 }
 
 export default App;
